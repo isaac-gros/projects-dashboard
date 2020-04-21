ca0c1b43972a09d3c36449be5630f72e4dd89f6a
Isaac Gros <isaac.gros888@gmail.com>
2020-04-22 00:21:58 +0200

    [FEAT] Navigation menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
-  <div id="app">
-    <nuxt/>
-  </div>
+	<div id="app">
+		<Menu v-if="$route.path !== '/'"/>
+		<nuxt/>
+	</div>
 </template>
+
+<script>
+import Menu from "~/components/dashboard/Menu"
+
+export default {
+	components: {
+		Menu
+	}
+}
+</script>
